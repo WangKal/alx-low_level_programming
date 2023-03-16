4b54fccc58a559473783b60fa99d5b9cc990ca11
Sam Otieno Owino <edgarsam.otieno@gmail.com>
2023-03-16 14:39:45 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
-int main(){
-	char strr[]="\"Programming is like building a multilingual puzzle";
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+	{
+
+	char strr[] = "\"Programming is like building a multilingual puzzle";
 	puts(strr);
-	return 0;
+	return (0);
+
 }
