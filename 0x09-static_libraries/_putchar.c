581468794de733e452e6d615b423f0b89978026b
Sam Otieno Owino <edgarsam.otieno@gmail.com>
2023-04-10 13:01:17 -0700

    m

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -2,20 +2,10 @@
 
 /**
   * main - Entry point
-  *
+  * @c: character
   * Return: Always 0
   */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
 	return (0);
 }
