d44ee1fa94e491430efba8afc986673664b90a60
=WangKal <=edgarsam.otieno@gmail.com>
2024-06-06 03:00:10 -0700

    m

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,15 @@
 #define _SEARCH_ALGOS_H_
 #include <stdio.h>
 #include <math.h>
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
